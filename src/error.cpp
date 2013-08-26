ac78c3b439931393d3f82bda77b3f49444fa596f
Dominik Lehmann <ZockDom@arcor.de>
2013-08-27 01:02:49 +0200

    Added error.cpp

---
+++ src/error.cpp
@@ -0,0 +1,25 @@
+
+/*
+  ScorePress - Music Engraving Software  (libscorepress)
+  Copyright (C) 2013 Dominik Lehmann
+  
+  Licensed under the EUPL, Version 1.1 or - as soon they
+  will be approved by the European Commission - subsequent
+  versions of the EUPL (the "Licence");
+  You may not use this work except in compliance with the
+  Licence.
+  
+  Unless required by applicable law or agreed to in
+  writing, software distributed under the Licence is
+  distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+  CONDITIONS OF ANY KIND, either expressed or implied.
+  See the Licence for the specific language governing
+  permissions and limitations under the Licence.
+*/
+
+#include "error.hh" // Error
+
+using namespace ScorePress;
+
+Error::Error() : std::string("Unknown Error within module \"libscorepress\"") {}
+Error::Error(const std::string msg) : std::string(msg) {}
